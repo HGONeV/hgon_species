78edd763af0a10ff38bc07f6f6488738e1bfdcd3
Maximilian Faessler <maximilian@faesslerweb.de>
2025-06-02 16:22:50 +0200

    [EDIT] code cleaning

--- Classes/Controller/SpeciesController.php
+++ Classes/Controller/SpeciesController.php
@@ -58,15 +58,6 @@ class SpeciesController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
             $speciesList = $this->speciesRepository->findAllOrderByName();
         }
 
-
-        // species type
-        /*
-        foreach ($speciesList as $species) {
-            $this->view->assign('speciesRecordType', $species->getRecordType());
-            break;
-        }
-        */
-
         $this->view->assign('speciesRecordType', $speciesRecordType);
         $this->view->assign('speciesList', $speciesList);
         //$this->view->assign('speciesCategories', $this->categoryRepository->findSubFamiliesOfExtendedFamilies($this->settings['parentCategoryUid']));
