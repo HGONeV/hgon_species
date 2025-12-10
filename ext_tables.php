e7bf33ab416fbaf22f2c40672624208513a3c95f
Maximilian Faessler <maximilian@faesslerweb.de>
2025-12-10 14:40:09 +0100

    migrate ext_tables to 9.5

--- ext_tables.php
+++ ext_tables.php
@@ -5,7 +5,6 @@ call_user_func(
     function($extKey)
     {
 
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('hgon_species', 'Configuration/TypoScript', 'HgonSpecies');
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_hgonspecies_domain_model_species', 'EXT:hgon_species/Resources/Private/Language/locallang_csh_tx_hgonspecies_domain_model_species.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_hgonspecies_domain_model_species');
